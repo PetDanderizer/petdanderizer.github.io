3441ab1d3041ee7598f43ce6074d95e378240985
PetDanderizer <megachode42069@gmail.com>
2025-10-25 17:15:38 -0500

    Delete week 9/Javascript Repetition Statements directory
    messed up capitalization, time to die

--- week 9/Javascript Repetition Statements/code.js
+++
@@ -1,9 +0,0 @@
-function colorMixer()
-{
-    var red = document.getElementById("red").value;
-    var greem = document.getElementById("greem").value;
-    var blue = document.getElementById("blue").value;
-
-    document.getElementById("sample").style = "background-color: rgb("+red+","+greem+","+blue+");";
-    document.getElementById("color-output").innerHTML= "background-color: rgb("+red+","+greem+","+blue+");";
-}
